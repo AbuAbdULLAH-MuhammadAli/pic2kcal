1573a80d16abc197289b756fc768b63308f336d0
lukasfrank <l.frank93@me.com>
2019-05-23 11:17:38 +0200

    Continued on training functionality

--- nn/dataset.py
+++ nn/dataset.py
@@ -1,14 +1,29 @@
 from torch.utils.data import Dataset
-from skimage import io, transform
+from torchvision import transforms, utils
 import json
 import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
 
 ROOT = "./data/"
 
 
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, kcal = sample['image'], sample['kcal']
+
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'kcal': torch.from_numpy(kcal)}
+
+
 class ImageCaloriesDataset(Dataset):
 
-    def __init__(self, calories_file, image_dir, transform=None):
+    def __init__(self, calories_file, image_dir, transform=transforms.Compose([ToTensor()])):
 
         with open(ROOT + calories_file) as json_file:
             self.calorie_image_tuples = json.load(json_file)["data"]
@@ -24,6 +39,20 @@ class ImageCaloriesDataset(Dataset):
         img_name = os.path.join(self.image_dir, element['name'])
 
         image = io.imread(img_name)
-        kcal = element['kcal']
+        kcal = np.array(element['kcal'])
 
-        return {'image': image, 'kcal': kcal}
+        sample = {'image': image, 'kcal': kcal}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    @staticmethod
+    def show_img_batch(sample_batched):
+        images_batch, kcal_batch = sample_batched['image'], sample_batched['kcal']
+
+        grid = utils.make_grid(images_batch)
+        plt.imshow(grid.numpy().transpose((1, 2, 0)))
+
+        plt.title('Samples')
--- nn/train.py
+++ nn/train.py
@@ -1,12 +1,43 @@
 from nn.models.res_net import ResNet
 from nn.dataset import ImageCaloriesDataset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+
+plt.ion()
+
+
 if __name__ == '__main__':
+    batch_size = 1
+    shuffle = True
 
     model = ResNet()
     net = model.get_model_on_device()
 
-    dataset = ImageCaloriesDataset('data.json', 'img')
+    train_dataset = ImageCaloriesDataset('train.json', 'train')
+    val_dataset = ImageCaloriesDataset('val.json', 'val')
 
-    print(net)
+    optimizer = optim.Adadelta(model.get_learnable_parameters())
+    criterion = nn.MSELoss()
 
+    for epoch in range(1):
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=shuffle)
 
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            optimizer.zero_grad()
+
+            outputs = net(data['image'])
+            loss = criterion(outputs, data['kcal'])
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if (i + 1) % 10 == 0:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 10))
+                running_loss = 0.0
+
+    model.save()
