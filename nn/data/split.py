1121ecbbcc674820c82a852aff78ae9b7008061f
verenaHeusser <veheusser@gmail.com>
2019-05-23 20:41:44 +0200

    Changing split.py

--- nn/data/split.py
+++ nn/data/split.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 from random import sample
 import pandas as pd
+from collections import defaultdict
 
 
 # read json
@@ -20,11 +21,16 @@ os.makedirs('train', exist_ok=True)
 os.makedirs('val', exist_ok=True)
 os.makedirs('test', exist_ok=True)
 
+output = defaultdict(list)
+
 for dataset, dsname in ((train, 'train'), (val, 'val'), (test, 'test')):
     for idx, row in dataset.iterrows():
         for i, img_name in enumerate(row['picture_files']):
             # print(img_name)
+            name = row['id']+'_'+str(i)+'.jpg'
+            output[dsname].append({'name': name, 'kcal': row['kcal_per_portion']})
             src = '../img/'+img_name
-            dest = dsname+'/'+row['id']+'_'+str(i)+'.jpg'
+            dest = dsname+'/'+name
             os.symlink(src, dest)
-
+    with open(dsname+'.json', 'w') as file:
+        json.dump({'data': output[dsname]}, file)
