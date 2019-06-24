fd039fd6f3bc1fdd357b274de0948127ebdcf7b5
lukasfrank <l.frank93@me.com>
2019-06-24 17:14:38 +0200

    Matched ingredients

--- extract_ingredients/main3.py
+++ extract_ingredients/main3.py
@@ -1,16 +1,22 @@
 import json
-from fuzzywuzzy import fuzz, process
 
-fddb_product_list = []
+from fuzzywuzzy import process
+from joblib import Parallel, delayed
+
+fddb_product_dict = {}
 ingredient_list = []
 
+# generate file with this command
+# jq -c   '{ key:(.name? // "asdf"), value: .name }  '  fddb.jsonl  | jq -s from_entries > fddb.json
 with open('./data/fddb.json') as file:
-    for fddb_product in file:
-        fddb_product_list.append(fddb_product.replace("\"", "").replace("\n", ""))
+    fddb_product_dict = json.load(file)
 
 with open('./data/out.json') as json_file:
     ingredient_list = json.load(json_file)
 
 
-for ingredient in ingredient_list:
-    print("{} : {}".format(ingredient, process.extract(ingredient, fddb_product_list, limit=3)))
+def extract(ingredient):
+    print("{} : {}".format(ingredient, process.extract(ingredient, fddb_product_dict, limit=3)))
+
+
+Parallel(n_jobs=4)(delayed(extract)(ingredient) for ingredient in ingredient_list)
