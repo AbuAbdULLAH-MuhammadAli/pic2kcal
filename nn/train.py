567c555f14d78fbd9505cb8e3f041f17a8bf9763
lukasfrank <l.frank93@me.com>
2019-07-06 15:37:22 +0200

    Remove plt

--- nn/train.py
+++ nn/train.py
@@ -61,7 +61,7 @@ def train():
     parser = argparse.ArgumentParser()
     parser.add_argument("--runname", help="name this experiment", required=True)
     args = parser.parse_args()
-    batch_size = 50
+    batch_size = 32
     epochs = 1
     shuffle = True
     validate_every = 100
