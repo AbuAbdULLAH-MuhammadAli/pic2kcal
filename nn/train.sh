a0eba3a95c385fba6a57ea26638b74659539d749
Praktikum 2019 <p19g3@i14s37.anthropomatik.kit.edu>
2019-07-11 00:05:50 +0200

    reg, log

--- nn/train.sh
+++ nn/train.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-PYTHONPATH=.. python3 train.py "$@" 2>&1 | tee "runs/$(date -Is).log"
+
+(echo cmdline "$0" "$@"; PYTHONPATH=.. python3 train.py "$@" 2>&1 ) | tee "runs/$(date -Is).log"
