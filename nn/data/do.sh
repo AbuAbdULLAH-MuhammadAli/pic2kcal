891f1a7cadf591172f7871844234bca0cc2e3a29
phiresky <phireskyde+git@gmail.com>
2019-07-14 20:15:54 +0200

    update split script

--- nn/data/do.sh
+++ nn/data/do.sh
@@ -1,4 +1,4 @@
 for kmode in per_recipe per_portion per_100g; do
     echo ------$kmode
-    python split.py --mode matched --kcal-mode $kmode --out-dir extracted_v1_$kmode
-done
+    python split.py --mode matched --kcal-mode $kmode --out-dir extracted_v2_$kmode
+done | tee -a extract.log
