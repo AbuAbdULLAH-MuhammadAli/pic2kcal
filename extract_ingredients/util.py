87491862d71c2ad6c889fdd87710738d1c2ae915
phiresky <phireskyde+git@gmail.com>
2019-07-14 20:16:04 +0200

    ingredient frequency matching notebook

--- extract_ingredients/util.py
+++ extract_ingredients/util.py
@@ -1,16 +1,31 @@
 import re
+
+
 def normalize_out_ingredient(ing: str):
-    regs = [r", frisch$", r", roh$", r", vom Huhn$", r", grün$"]
+    regs = []  # [r", frisch$", r", roh$", r", vom Huhn$", r", grün$"]
+
     ing = ing.strip()
     outs = {ing}
+    while True:
+        # Zucchini, grün, frisch -> Zucchini UND Zucchini, grün UND Zucchini, grün, frisch
+        ing2 = re.sub(r",[^,]+$", "", ing)
+        if ing == ing2:
+            break
+        ing = ing2
+        outs.add(ing)
+
+    additions = {
+        "Ei": ["Eier"],
+        "Salz": ["Salz und Pfeffer"],
+        "Paprika": ["Paprikaschote"],
+        "Knoblauch": ["Knoblauchzehe"],
+        "Karotten / Möhren": ["Karotten", "Karotte", "Möhren", "Möhre"],
+        "Lorbeerblatt": ["Lorbeer"],
+        "Apfel": ["Äpfel"]
+    }
     for reg in regs:
         ing = re.sub(reg, "", ing)
         outs.add(ing)
-    if ing == "Ei":
-        outs.add("Eier")
-    if ing == "Salz":
-        outs.add("Salz und Pfeffer")
-    if ing == "Karotten / Möhren":
-        outs.add("Karotten")
-        outs.add("Möhren")
+    if ing in additions:
+        outs.update(additions[ing])
     return list(outs)
