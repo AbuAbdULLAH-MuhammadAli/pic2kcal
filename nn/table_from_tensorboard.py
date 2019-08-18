fc2c32df48d077b22efb713cd13b5d9e0c6b5056
phiresky <phireskyde+git@gmail.com>
2019-08-18 16:34:23 +0200

    convert tensorboard to latex table automatically

---
+++ nn/table_from_tensorboard.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from collections import defaultdict
+import numpy
+import pandas
+import sys
+import glob
+from pathlib import Path
+values = defaultdict(list)
+
+
+def box_filt(n):
+    return numpy.array([1] * n, dtype="float32") / n
+
+
+def do_single(path: Path):
+    [path] = path.glob("events.out.*")
+    avg_width = 50
+    for e in tf.train.summary_iterator(str(path)):
+        for v in e.summary.value:
+            if v.tag.startswith("val_") and not v.image.width:
+                values[v.tag] += [v.simple_value]
+
+    table = {}
+
+    for k, v in values.items():
+        v = numpy.asarray(v)
+        bf = box_filt(avg_width)
+        v_filt = numpy.convolve(v, bf, mode='valid')
+        #print(v_filt)
+        table[k] = min(v_filt)
+    return table
+
+
+def run_name(path):
+    return Path(path).name
+
+all_data = {run_name(run): do_single(Path(run)) for run in sys.argv[1:]}
+
+df = pandas.DataFrame.from_dict(all_data, orient='index')
+print(df)
+print(df.to_latex())
